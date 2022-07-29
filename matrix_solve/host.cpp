ecc86fd8fb4b0a863fd6ef8d2e5db07afee40f82
shawlleyw <wsyy0619@gmail.com>
2022-07-29 17:40:02 +0800

    feat: naive para, negative optimization

--- matrix_solve/host.cpp
+++ matrix_solve/host.cpp
@@ -2,8 +2,20 @@
 #include "matrix_def.h"
 #include "host.h"
 
+struct Para {
+    double *vec;
+    double *mat_val;
+    double *res;
+    int *cols;
+    int *block;
+    int *r_ptr;
+};
 
-void spmv(const CsrMatrix &csr_matrix, double *x, double *b) {
+extern "C" {
+    void slave_func_calc(Para *x);
+}
+
+void naive_spmv(const CsrMatrix &csr_matrix, double *x, double *b) {
     // 实现样例
     for(int i = 0; i < csr_matrix.rows; i++) {
         int start = csr_matrix.row_off[i];
@@ -14,4 +26,37 @@ void spmv(const CsrMatrix &csr_matrix, double *x, double *b) {
         }
         b[i]=result;
     }
+}
+
+
+void spmv(const CsrMatrix &csr_matrix, double *x, double *b) {
+    int n = csr_matrix.rows;
+    int nnz = csr_matrix.row_off[n];
+    double *val = csr_matrix.data;
+    int *cols = csr_matrix.cols;
+    int *r_ptr = csr_matrix.row_off;
+
+    const int nt = 64;
+
+    int load = n / nt;
+    int *block = (int*)malloc(sizeof(int)*(nt+1));
+    for(int i = 0; i < nt; i++) {
+        block[i] = load * i;
+    }
+    block[nt] = n;
+
+    Para arg = Para{};
+    arg.vec = x;
+    arg.mat_val = val;
+    arg.res = b;
+    arg.cols = cols;
+    arg.block = block;
+    arg.r_ptr = r_ptr;
+    CRTS_init();
+    CRTS_athread_spawn(slave_func_calc, &arg);
+    CRTS_athread_join();
+    CRTS_athread_halt();
+
+    free(block);
+
 }
--- matrix_solve/slave.c
+++ matrix_solve/slave.c
@@ -1,3 +1,27 @@
 #include <slave.h>
 #include "matrix_def.h"
 
+struct Para {
+    double *vec;
+    double *mat_val;
+    double *res;
+    int *cols;
+    int *block;
+    int *r_ptr;
+};
+
+
+void slave_func_calc(struct Para *x) {
+    int tid = CRTS_tid;
+    int start = x->block[tid];
+    int end = x->block[tid+1];
+    for(int i = start; i < end; i++) {
+        int l = x->r_ptr[i];
+        int r = x->r_ptr[i+1];
+        double res = .0;
+        for(int j = l; j < r; j++) {
+            res += x->vec[x->cols[j]] * x->mat_val[j];
+        }
+        x->res[i] = res;
+    }
+}
