1df4947ad49e737ff47df3be81cc2e0ff2ea11e8
lijiamu233 <1127915415@qq.com>
2022-07-29 15:06:58 +0800

    fix: Index 6 no error

--- matrix_solve/host.cpp
+++ matrix_solve/host.cpp
@@ -1,72 +1,67 @@
-#include <athread.h>
-#include "matrix_def.h"
 #include "host.h"
-
+#include "matrix_def.h"
+#include <athread.h>
 
 const int thread_num = 64;
 extern "C" {
-    void slave_func_albus_part1(void*);
-    void slave_func_albus_part2(void*);
+    void slave_func_albus_part1(void *);
+    void slave_func_albus_part2(void *);
 }
 
 void naive_spmv(const CsrMatrix &csr_matrix, double *x, double *b) {
     // 实现样例
-    for(int i = 0; i < csr_matrix.rows; i++) {
-        int start = csr_matrix.row_off[i];
-        int num = csr_matrix.row_off[i+1] - csr_matrix.row_off[i];
-        double result = .0;
-        for(int j = 0; j < num; j++) {
-            result += x[csr_matrix.cols[start+j]] * csr_matrix.data[start+j];
-        }
-        b[i]=result;
+    for (int i = 0; i < csr_matrix.rows; i++) {
+      int start = csr_matrix.row_off[i];
+      int num = csr_matrix.row_off[i + 1] - csr_matrix.row_off[i];
+      double result = .0;
+      for (int j = 0; j < num; j++) {
+        result += x[csr_matrix.cols[start + j]] * csr_matrix.data[start + j];
+      }
+      b[i] = result;
     }
-    
 }
 
 int GetRowIdx(int n, int x, int *off) {
     int l = 0;
-    int r = n-1;
-    if(x >= off[n-1]) return n-1;
-    while(l < r) {
-        int mid = (l+r+1) >> 1;
-        if(off[mid] > x) {
-            r = mid - 1;
-        } else {
-            l = mid;
-        }
+    int r = n - 1;
+    if (x >= off[n - 1])
+      return n - 1;
+    while (l < r) {
+      int mid = (l + r + 1) >> 1;
+      if (off[mid] > x) {
+        r = mid - 1;
+      } else {
+        l = mid;
+      }
     }
     return l;
 }
 
-
 void spmv(const CsrMatrix &csr_matrix, double *x, double *b) {
     // naive_spmv(csr_matrix, x, b);
-    int nnz = csr_matrix.data_size;
     int row_num = csr_matrix.rows;
+    int nnz = csr_matrix.row_off[row_num];
     int *col_idx = csr_matrix.cols;
     double *mtx_val = csr_matrix.data;
     int *row_ptr = csr_matrix.row_off;
-    
-    
 
-    int *start = (int*)malloc(sizeof(int)*thread_num);
-    int *end = (int*)malloc(sizeof(int)*(thread_num+1));
-    int *block_size = (int*)malloc(sizeof(int)*thread_num);
-    double *mid_ans = (double*)malloc(sizeof(double)*thread_num*2);
+    int *start = (int *)malloc(sizeof(int) * thread_num);
+    int *end = (int *)malloc(sizeof(int) * (thread_num + 1));
+    int *block_size = (int *)malloc(sizeof(int) * (thread_num + 1));
+    double *mid_ans = (double *)malloc(sizeof(double) * thread_num * 2);
 
-    //Algorithm 4 : ALBUS load balancing settings
+    // Algorithm 4 : ALBUS load balancing settings
     block_size[thread_num] = nnz;
     start[0] = 0;
     end[thread_num - 1] = row_num;
     int t = nnz / thread_num;
-    for(int i = 0; i < thread_num; i++) {
-        block_size[i] = i * t;
-        start[i] = GetRowIdx(row_num, block_size[i], row_ptr);
-        end[i - 1] = start[i];
+    for (int i = 0; i < thread_num; i++) {
+      block_size[i] = i * t;
+      start[i] = GetRowIdx(row_num, block_size[i], row_ptr);
+      end[i - 1] = start[i];
     }
 
     // start & end: edge case?
-
 
     // start slave func
     AlbusArgs args = AlbusArgs{};
